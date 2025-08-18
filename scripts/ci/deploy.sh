2092a0aea6b538c4fa2407016a09c8aa5761ab5d
Francesco Sardone <francesco@airscript.it>
2025-08-18 14:13:05 +0000

    fix: add missing vercel pull

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-vercel build
+vercel pull --yes --environment=production --token $3
+vercel build --token $3
 VERCEL_ORG_ID=$1 VERCEL_PROJECT_ID=$2 vercel deploy --prebuilt --token $3 --prod --yes
