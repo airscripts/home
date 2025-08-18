66191ab9ca6e1c14a9a78ccef596e1968a2226fa
Francesco Sardone <francesco@airscript.it>
2025-08-18 14:24:07 +0000

    refactor: avoid prebuilding locally

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-vercel pull --yes --environment=production --token $3
-vercel build --environment=production --token $3
-VERCEL_ORG_ID=$1 VERCEL_PROJECT_ID=$2 vercel deploy --prebuilt --token $3 --prod --yes
+VERCEL_ORG_ID=$1 VERCEL_PROJECT_ID=$2 vercel deploy --token $3 --prod --yes
