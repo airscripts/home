16c18a70b1ce2a47c0a01cd83149225e608a756e
Francesco Sardone <francesco@airscript.it>
2025-08-18 14:21:06 +0000

    fix: add missing build environment

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 vercel pull --yes --environment=production --token $3
-vercel build --token $3
+vercel build --environment=production --token $3
 VERCEL_ORG_ID=$1 VERCEL_PROJECT_ID=$2 vercel deploy --prebuilt --token $3 --prod --yes
